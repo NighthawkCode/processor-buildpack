3d5b5e66258854dde809693d6d6cb5c6b1238bc2
Ryan <ryan@tesseraktinstitute.org>
2024-10-02 12:49:41 -0700

    use short version of commit hash, language

--- heroku-build.sh
+++ heroku-build.sh
@@ -3,10 +3,10 @@
 echo "Capturing SOURCE_VERSION during build phase."
 
 if [ -z "$SOURCE_VERSION" ]; then
-  echo "SOURCE_VERSION not found, setting commit hash to 'unknown'."
-  commit_hash="unknown"
+  echo "SOURCE_VERSION not found, setting commit hash to 'local'."
+  commit_hash="local"
 else
-  commit_hash="$SOURCE_VERSION"
+  commit_hash="${SOURCE_VERSION:0:7}"
   echo "Commit hash from SOURCE_VERSION: $commit_hash"
 fi
 
